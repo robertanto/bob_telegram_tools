20a0d86fff06f5d9da1ebbf029d8ad8991379351
antonio <a.roberto11@studenti.unisa.it>
2020-03-22 20:38:04 +0100

    BobTelegramTools Released
    V1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bob-telegram-tools",  # Replace with your own username
-    version="0.0.1",
+    version="1.0.0",
     author="Antonio Roberto",
     author_email="roberto.antonio@outlook.it",
     description="A package to monitor your Machine Learning trainings every where without any additional app.",
